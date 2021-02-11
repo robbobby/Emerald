5ff9b620441166118ecda36d3dd732c0d6dd7e7a
robbobby <69080884+robbobby@users.noreply.github.com>
2021-02-11 00:56:33 +0000

    Switched Ui keybinds to input system - need to disable on chat

--- Emerald/Assets/Scripts/UiWindowController.cs
+++ Emerald/Assets/Scripts/UiWindowController.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UiWindowController : MonoBehaviour {
     public GameObject GfxMenu;
@@ -10,74 +10,48 @@ public class UiWindowController : MonoBehaviour {
     public GameObject SkillsMenu;
     public GameObject GuildMenu;
     public GameObject OptionsMenu;
-    public GameObject MiniMap;
+    public GameObject MiniMap;\
+    public InputController.UIActions UiInput;
 
-    private KeyCode lastPressedKey;
-    private bool newEventTrigger;
-
-    public void Update() {
-        if (!Input.anyKeyDown || !newEventTrigger) return;
-        HandleKeyDown();
-        newEventTrigger = false;
+    private void Awake() {
+        UiInput = new InputController().UI;
+        UiInput.Inventory.performed += inventoryCallback => InventoryWindowStateHandler();
+        UiInput.Character.performed += inventoryCallback => CharacterWindowStateHandler();
+        UiInput.Options.performed += inventoryCallback => OptionWindowStateHandler();
+        UiInput.Skills.performed += inventoryCallback => SkillWindowStateHandler();
+        UiInput.Guild.performed += inventoryCallback => GuildWindowStateHandler();
+        UiInput.MiniMap.performed += inventoryCallback => MiniMapWindowStateHandler();
     }
 
-    public void OnGUI() {
-        if (!Input.anyKeyDown || Event.current.keyCode == KeyCode.None) return;
-        lastPressedKey = Event.current.keyCode;
-        newEventTrigger = Event.current.isKey;
+    private void GuildWindowStateHandler() {
+        GuildMenu.SetActive(!GuildMenu.activeSelf);
     }
 
-    private void HandleKeyDown() {
-        Debug.Log(lastPressedKey);
-        switch (lastPressedKey) {
-            case KeyCode.None:
-                break;
-            case KeyCode.F9:
-                InventoryMenu.SetActive(!InventoryMenu.activeSelf);
-                break;
-            case KeyCode.F10:
-                CharacterMenu.SetActive(!CharacterMenu.activeSelf);
-                break;
-            case KeyCode.F11:
-                SkillsMenu.SetActive(!SkillsMenu.activeSelf);
-                break;
-            case KeyCode.F12:
-                OptionsMenu.SetActive(!OptionsMenu.activeSelf);
-                break;
-            case KeyCode.C:
-                CharacterMenu.SetActive(!CharacterMenu.activeSelf);
-                break;
-            case KeyCode.G:
-                GuildMenu.SetActive(!GuildMenu.activeSelf);
-                break;
-            case KeyCode.I:
-                InventoryMenu.SetActive(!InventoryMenu.activeSelf);
-                break;
-            case KeyCode.V:
-                MiniMap.SetActive(!MiniMap.activeSelf);
-                break;
-            case KeyCode.K:
-                SkillsMenu.SetActive(!SkillsMenu.activeSelf);
-                // Rank
-                break;
-            case KeyCode.M:
-                // Mail
-                break;
-            case KeyCode.P:
-                // Group
-                break;
-            case KeyCode.Q:
-                // Quest
-                break;
-            case KeyCode.R:
-                // Mount
-                break;
-            case KeyCode.Y:
-                // Game shop
-                break;
-            default:
-                Debug.Log("Something just went wrong with the UI Controller");
-                throw new ArgumentOutOfRangeException();
-        }
+    private void MiniMapWindowStateHandler() {
+        MiniMap.SetActive(!MiniMap.activeSelf);
+    }
+
+    private void SkillWindowStateHandler() {
+        SkillsMenu.SetActive(!SkillsMenu.activeSelf);
+    }
+
+    private void OptionWindowStateHandler() {
+        OptionsMenu.SetActive(!OptionsMenu.activeSelf);
+    }
+
+    private void CharacterWindowStateHandler() {
+        CharacterMenu.SetActive(!CharacterMenu.activeSelf);
+    }
+
+    private void InventoryWindowStateHandler() {
+        InventoryMenu.SetActive(!InventoryMenu.activeSelf);
+    }
+
+    private void OnEnable() {
+        UiInput.Enable();
+    }
+
+    private void OnDisable() {
+        UiInput.Disable();
     }
 }
